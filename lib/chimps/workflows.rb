426a662821c0288fdcbaa829d321f2a8cf1f53d7
Dhruv Bansal <dhruv@ph.utexas.edu>
2010-05-20 08:11:34 -0500

    More complete autoloading.

--- lib/chimps/workflows.rb
+++ lib/chimps/workflows.rb
@@ -1,8 +1,11 @@
 module Chimps
+
+  # A module defining classes to handle complex workflows between the
+  # local machine and Infochimps' servers.
   module Workflows
-    autoload :Uploader,   'chimps/workflows/uploader'
-    autoload :Downloader, 'chimps/workflows/downloader'
-    autoload :Batch,      'chimps/workflows/batch'
+    autoload :Uploader,     'chimps/workflows/uploader'
+    autoload :Downloader,   'chimps/workflows/downloader'
+    autoload :BatchUpdater, 'chimps/workflows/batch'
   end
 end
 
